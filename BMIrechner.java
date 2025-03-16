1b52b9699f278ded11873f3e1cf3973cfbc999b1
Melvin <schmitt.melvin@gmail.com>
2025-03-16 15:07:05 +0100

    BMIrechner.java

---
+++ BMIrechner.java
@@ -0,0 +1,18 @@
+package com.BMI;
+import java.util.Scanner;
+
+public class BMIrechner_ohne {
+
+    public static void main(String[] args) {
+        Scanner enter = new Scanner(System.in);
+
+        System.out.println("Größe in Metern, im Format 0,00:");
+        double height = enter.nextDouble();
+        System.out.println("Gewicht in kg, im Format 0,00:");
+        double weight = enter.nextDouble();
+
+        double summary = (weight / (height * height));
+        double summrun = Math.round(summary);
+        System.out.println("Ihr BMI beträgt: " + summrun + "!");
+    }
+}
